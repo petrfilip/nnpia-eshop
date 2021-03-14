6f0f329a594f80319e7d6acc0d1f9a6f562c1860
pavel.jetensky@seznam.cz <pavel.jetensky@seznam.cz>
2021-03-14 18:57:56 +0100

    Step 2

--- src/test/java/cz/upce/eshop/ProductRepositoryTest.java
+++ src/test/java/cz/upce/eshop/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package cz.upce.eshop;
 
+import cz.upce.eshop.datafactory.ProductTestDataFactory;
 import cz.upce.eshop.entity.Product;
 import cz.upce.eshop.repository.ProductRepository;
 import java.util.List;
@@ -10,29 +11,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Import(ProductTestDataFactory.class)
 class ProductRepositoryTest {
 
   @Autowired
   private ProductRepository productRepository;
 
+  @Autowired
+  ProductTestDataFactory productTestDataFactory;
+
   @Test
   void saveProductTest() {
 
-    saveProduct();
+    productTestDataFactory.saveProduct("MyProduct");
     List<Product> all = productRepository.findAll();
     Assertions.assertThat(all.size()).isEqualTo(1);
 
   }
 
-  private void saveProduct() {
-    Product product = new Product();
-    product.setProductName("MyProduct");
-    productRepository.save(product);
-  }
 
 }
--- src/test/java/cz/upce/eshop/datafactory/ProductTestDataFactory.java
+++ src/test/java/cz/upce/eshop/datafactory/ProductTestDataFactory.java
@@ -1,5 +1,20 @@
 package cz.upce.eshop.datafactory;
 
 
+import cz.upce.eshop.entity.Product;
+import cz.upce.eshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductTestDataFactory {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void saveProduct(String productName) {
+        Product product = new Product();
+        product.setProductName(productName);
+        productRepository.save(product);
+    }
 }
