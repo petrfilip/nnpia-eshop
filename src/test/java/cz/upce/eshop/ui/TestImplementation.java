98e7aba7471f53cfabe6c387a8a41b1ef9321e5c
pavel.jetensky@seznam.cz <pavel.jetensky@seznam.cz>
2021-03-25 22:10:15 +0100

    Hello world selenium test

---
+++ src/test/java/cz/upce/eshop/ui/TestImplementation.java
@@ -0,0 +1,40 @@
+package cz.upce.eshop.ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestImplementation {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupWebdriverChromeDriver() {
+        String chromedriverPath =  TestImplementation.class.getResource("/chromedriver.exe").getFile();
+        System.setProperty("webdriver.chrome.driver", chromedriverPath);
+    }
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void verifyGitHubTitle() {
+        driver.get("https://www.github.com");
+        assertThat(driver.getTitle(), containsString("GitHub"));
+    }
+}
